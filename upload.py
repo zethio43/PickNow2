21a145e051615baf7de1ce03e080be3a2d2c5037
Unknown <tanutpueon@gmail.com>
2018-01-17 15:23:13 +0700

    Commit 15:23

--- upload.py
+++ upload.py
@@ -6,14 +6,12 @@ Created on Mon Jan 15 16:37:20 2018
 @author: earthz
 """
 
-# import requests
 import os
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'img'
-# CUTBG_FOLDER ='cbg'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
@@ -27,10 +25,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# def api(filename):
-#     files = {'file': open(filename, 'rb')}
-#     requests.post(url, files=files)
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
@@ -38,15 +32,12 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # api(filename)
     return render_template('index2.html', image_name=filename)
 
 @app.route('/upload/<filename>')
 def send_image(filename):
     return send_from_directory("UPLOAD_FOLDER",filename)
 
-# @app.route('')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(port='5000')
+    app.run(port=4555, debug=True)
