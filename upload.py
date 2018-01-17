e1faf98e62b5e534fb544dd39cef4d1f16f8e29b
Unknown <tanutpueon@gmail.com>
2018-01-17 13:51:19 +0700

    Commit

--- upload.py
+++ upload.py
@@ -6,8 +6,10 @@ Created on Mon Jan 15 16:37:20 2018
 @author: earthz
 """
 
+import requests
 import os
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'img'
